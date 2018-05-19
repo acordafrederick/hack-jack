2bf5f54f8a2365ea9a833145b2a9851f7b8e4dfb
acordafrederick <sonjaire@gmail.com>
2018-05-19 02:08:29 -0500

    created new html lay-out, created svg playing cards

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,105 +1,102 @@
+// $(document).ready(function () {
 
-$(document).ready(function() {
+//     // GENERATE RANDOM NUMBER ====================
+//     let number = Math.floor(Math.random() * 101 + 19);
+//     $("#randomNumber").text(number);
 
-// GENERATE RANDOM NUMBER ====================
-  var number = Math.floor(Math.random() * 101 + 19);
-  $("#randomNumber").text(number);
+//     // OTHER GLOBALS ======================
+//     var yourScore = 0;
+//     var wins = 0;
+//     var losses = 0;
+//     var winner = "YOU WON!";
+//     var loser = "YOU LOST!";
 
-// OTHER GLOBALS ======================
-  var yourScore = 0;
-  var wins = 0;
-  var losses = 0;
-  var winner = "YOU WON!";
-  var loser = "YOU LOST!";
+//     // CARDS TO GENERATE RANDOM VALUES ========================
+//     var cards = {
+//         "spade": Math.floor(Math.random() * 11 + 1),
+//         "heart": Math.floor(Math.random() * 11 + 1),
+//         "club": Math.floor(Math.random() * 11 + 1),
+//         "diamond": Math.floor(Math.random() * 11 + 1)
+//     }
 
-// CARDS TO GENERATE RANDOM VALUES ========================
-  var cards = {
-      "spade" : Math.floor(Math.random() * 11 + 1),
-      "heart" : Math.floor(Math.random() * 11 + 1),
-      "club" : Math.floor(Math.random() * 11 + 1),
-      "diamond" : Math.floor(Math.random() * 11 + 1)
-    }
+//     // RESET / STARTING FUNCTION
+//     function reset() {
+//         number = Math.floor(Math.random() * 101 + 19);
+//         $('#randomNumber').text(number);
+//         cards = {
+//             "spade": Math.floor(Math.random() * 11 + 1),
+//             "heart": Math.floor(Math.random() * 11 + 1),
+//             "club": Math.floor(Math.random() * 11 + 1),
+//             "diamond": Math.floor(Math.random() * 11 + 1)
+//         }
+//         yourScore = 0;
+//     }
 
-    // RESET / STARTING FUNCTION
-  function reset(){
-    number = Math.floor(Math.random()*101+19);
-    $('#randomNumber').text(number);
-    cards = {
-      "spade" : Math.floor(Math.random() * 11 + 1),
-      "heart" : Math.floor(Math.random() * 11 + 1),
-      "club" : Math.floor(Math.random() * 11 + 1),
-      "diamond" : Math.floor(Math.random() * 11 + 1)
-    }
-    yourScore = 0;
-    }
+//     // SET FUNCTIONS FOR WHEN THE PLAYER WINS OR LOSES
+//     function winning() {
+//         wins++;
+//         $("#wins").text(wins);
+//         $("#message").text(winner);
+//         $("#yourScore").empty();
+//     }
 
-// SET FUNCTIONS FOR WHEN THE PLAYER WINS OR LOSES
-  function winning() {
-    wins++;
-    $("#wins").text(wins);
-    $("#message").text(winner);
-    $("#yourScore").empty();
-  }
+//     function lose() {
+//         losses++;
+//         $("#losses").text(losses);
+//         $("#message").text(loser);
+//         $("#yourScore").empty();
+//     }
 
-  function lose() {
-    losses++;
-    $("#losses").text(losses);
-    $("#message").text(loser);
-    $("#yourScore").empty();
-  }
+//     //  MAKE THE CARDS CLICKABLE AND ITS VALUES APPEAR / CONDITIONS ======================
+//     $("#spade").on("click", function () {
+//         yourScore = yourScore + cards.spade;
+//         $("#yourScore").text(yourScore);
 
-//  MAKE THE CARDS CLICKABLE AND ITS VALUES APPEAR / CONDITIONS ======================
-  $("#spade").on("click", function(){
-    yourScore = yourScore + cards.spade;
-    $("#yourScore").text(yourScore);
-    
-    if (yourScore === number) {
-      winning();
-      reset();
-    }
-    else if (yourScore > number) {
-      lose();
-      reset();
-    }
-  });
-  $("#heart").on("click", function(){
-    yourScore = yourScore + cards.heart;
-    $("#yourScore").text(yourScore);
+//         if (yourScore === number) {
+//             winning();
+//             reset();
+//         } else if (yourScore > number) {
+//             lose();
+//             reset();
+//         }
+//     });
 
-    if (yourScore === number) {
-      winning();
-      reset();
-    }
-    else if (yourScore > number) {
-      lose();
-      reset();
-    }
-  });
-  $("#club").on("click", function(){
-    yourScore = yourScore + cards.club;
-    $("#yourScore").text(yourScore);
+//     $("#heart").on("click", function () {
+//         yourScore = yourScore + cards.heart;
+//         $("#yourScore").text(yourScore);
 
-    if (yourScore === number) {
-      winning();
-      reset();
-    }
-    else if (yourScore > number) {
-     lose();
-      reset();
-    }
-  });
-  $("#diamond").on("click", function(){
-    yourScore = yourScore + cards.diamond;
-    $("#yourScore").text(yourScore);
+//         if (yourScore === number) {
+//             winning();
+//             reset();
+//         } else if (yourScore > number) {
+//             lose();
+//             reset();
+//         }
+//     });
+//     $("#club").on("click", function () {
+//         yourScore = yourScore + cards.club;
+//         $("#yourScore").text(yourScore);
 
-    if (yourScore === number) {
-     winning();
-      reset();
-    }
-    else if (yourScore > number) {
-      lose();
-      reset();
-    }
-  });
+//         if (yourScore === number) {
+//             winning();
+//             reset();
+//         } else if (yourScore > number) {
+//             lose();
+//             reset();
+//         }
+//     });
+//     $("#diamond").on("click", function () {
+//         yourScore = yourScore + cards.diamond;
+//         $("#yourScore").text(yourScore);
 
-});
+//         if (yourScore === number) {
+//             winning();
+//             reset();
+//         } else if (yourScore > number) {
+//             lose();
+//             reset();
+//         }
+//     });
+
+// });
+
