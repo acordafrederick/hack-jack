c0a9fb8c3e2fd1127be5cb320f7b2b1a6ed42277
acordafrederick <sonjaire@gmail.com>
2018-05-20 00:01:47 -0500

    finished the initial layout; added some styling and media queries; incorporated functionality.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,102 +1,87 @@
-// $(document).ready(function () {
+$(document).ready(function () {
 
-//     // GENERATE RANDOM NUMBER ====================
-//     let number = Math.floor(Math.random() * 101 + 19);
-//     $("#randomNumber").text(number);
+    // GENERATE RANDOM NUMBER ====================
+    let number = Math.floor(Math.random() * 101 + 19);
+    $("#number").text(number);
+    $("#score").text("0");
 
-//     // OTHER GLOBALS ======================
-//     var yourScore = 0;
-//     var wins = 0;
-//     var losses = 0;
-//     var winner = "YOU WON!";
-//     var loser = "YOU LOST!";
+    // OTHER GLOBALS ======================
+    let score = 0;
+    let wins = 0;
+    let losses = 0;
+    const winMessage = "It's a match! Keep it up!";
+    const loseMessage = "Not a match! Try again!";
+    const introMessage = "Match the Number";
 
-//     // CARDS TO GENERATE RANDOM VALUES ========================
-//     var cards = {
-//         "spade": Math.floor(Math.random() * 11 + 1),
-//         "heart": Math.floor(Math.random() * 11 + 1),
-//         "club": Math.floor(Math.random() * 11 + 1),
-//         "diamond": Math.floor(Math.random() * 11 + 1)
-//     }
+    // CARDS TO GENERATE RANDOM VALUES ========================
+    var cards = {
+        "spade": Math.floor(Math.random() * 11 + 1),
+        "heart": Math.floor(Math.random() * 11 + 1),
+        "club": Math.floor(Math.random() * 11 + 1),
+        "diamond": Math.floor(Math.random() * 11 + 1)
+    }
 
-//     // RESET / STARTING FUNCTION
-//     function reset() {
-//         number = Math.floor(Math.random() * 101 + 19);
-//         $('#randomNumber').text(number);
-//         cards = {
-//             "spade": Math.floor(Math.random() * 11 + 1),
-//             "heart": Math.floor(Math.random() * 11 + 1),
-//             "club": Math.floor(Math.random() * 11 + 1),
-//             "diamond": Math.floor(Math.random() * 11 + 1)
-//         }
-//         yourScore = 0;
-//     }
+    // RESET / STARTING FUNCTION
+    function reset() {
+        number = Math.floor(Math.random() * 101 + 19);
+        $("#number").text(number);
+        cards = {
+            "spade": Math.floor(Math.random() * 11 + 1),
+            "heart": Math.floor(Math.random() * 11 + 1),
+            "club": Math.floor(Math.random() * 11 + 1),
+            "diamond": Math.floor(Math.random() * 11 + 1)
+        }
+        score = 0;
+    }
 
-//     // SET FUNCTIONS FOR WHEN THE PLAYER WINS OR LOSES
-//     function winning() {
-//         wins++;
-//         $("#wins").text(wins);
-//         $("#message").text(winner);
-//         $("#yourScore").empty();
-//     }
+    // SET FUNCTIONS FOR WHEN THE PLAYER WINS OR LOSES
+    function winning() {
+        wins++;
+        $("#wins").text(wins);
+        $("#score").text("0");
+        $("#message").text(winMessage);
+    }
 
-//     function lose() {
-//         losses++;
-//         $("#losses").text(losses);
-//         $("#message").text(loser);
-//         $("#yourScore").empty();
-//     }
+    function lose() {
+        losses++;
+        $("#losses").text(losses);
+        $("#score").text("0");
+        $("#message").text(loseMessage);
+    }
 
-//     //  MAKE THE CARDS CLICKABLE AND ITS VALUES APPEAR / CONDITIONS ======================
-//     $("#spade").on("click", function () {
-//         yourScore = yourScore + cards.spade;
-//         $("#yourScore").text(yourScore);
+    function matchNumbers() {
+        if (score === number) {
+            winning();
+            reset();
+        } else if (score > number) {
+            lose();
+            reset();
+        }
+    }
 
-//         if (yourScore === number) {
-//             winning();
-//             reset();
-//         } else if (yourScore > number) {
-//             lose();
-//             reset();
-//         }
-//     });
+    //  MAKE THE CARDS CLICKABLE AND ITS VALUES APPEAR / CONDITIONS ======================
+    $("#spades").on("click", function () {
+        score = score + cards.spade;
+        $("#score").text(score);
+        matchNumbers();
+    });
 
-//     $("#heart").on("click", function () {
-//         yourScore = yourScore + cards.heart;
-//         $("#yourScore").text(yourScore);
+    $("#hearts").on("click", function () {
+        score = score + cards.heart;
+        $("#score").text(score);
+        matchNumbers();
+    });
+    $("#clubs").on("click", function () {
+        score = score + cards.club;
+        $("#score").text(score);
+        matchNumbers();
+    });
 
-//         if (yourScore === number) {
-//             winning();
-//             reset();
-//         } else if (yourScore > number) {
-//             lose();
-//             reset();
-//         }
-//     });
-//     $("#club").on("click", function () {
-//         yourScore = yourScore + cards.club;
-//         $("#yourScore").text(yourScore);
+    $("#diamonds").on("click", function () {
+        score = score + cards.diamond;
+        $("#score").text(score);
+        matchNumbers();
+    });
 
-//         if (yourScore === number) {
-//             winning();
-//             reset();
-//         } else if (yourScore > number) {
-//             lose();
-//             reset();
-//         }
-//     });
-//     $("#diamond").on("click", function () {
-//         yourScore = yourScore + cards.diamond;
-//         $("#yourScore").text(yourScore);
-
-//         if (yourScore === number) {
-//             winning();
-//             reset();
-//         } else if (yourScore > number) {
-//             lose();
-//             reset();
-//         }
-//     });
-
-// });
+});
 
