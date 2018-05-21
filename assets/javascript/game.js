c7c9c3c5c79e9f75e52bc01db2b3e8e3a146aa2f
acordafrederick <sonjaire@gmail.com>
2018-05-21 02:42:48 -0500

    added the elements info-container and open-info; made some polishing on styling; finished the game's logic, and tied up click evens for the users.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,36 @@
+// hide the ff elements for intro
+$("#game-container, #open-info, #close-info").hide();
+$("#info-container").hide();
+
 $(document).ready(function () {
+
+    $("body").addClass("logo-bg");
+    setTimeout(function() {
+        $("body").removeClass("logo-bg");
+        $("#info-container").slideToggle().show();
+    },5000);
+
+    $("#play-btn").click(function() {
+        $("#info-container").slideToggle();
+        setTimeout(function() {
+            $("#game-container, #open-info").show();
+        }, 500);
+    });
+
+    $("#open-info").click(function() {
+        $("#game-container").fadeTo(0, 0);
+        $("#play-btn, #open-info").hide();
+        $("#info-container").slideToggle().show();
+        $("#close-info").show();
+    });
+
+    $("#close-info").click(function() {
+        $("#info-container").slideToggle();
+        setTimeout(function() {
+            $("#game-container").fadeTo(0, 1);
+            $("#open-info").show();
+        }, 500);
+    })
 
     // GENERATE RANDOM NUMBER ====================
     let number = Math.floor(Math.random() * 101 + 19);
@@ -9,8 +41,8 @@ $(document).ready(function () {
     let score = 0;
     let wins = 0;
     let losses = 0;
-    const winMessage = "It's a match! Keep it up!";
-    const loseMessage = "Not a match! Try again!";
+    const winMessage = "It's a match. Keep it up!";
+    const loseMessage = "Not a match. Try again!";
     const introMessage = "Match the Number";
 
     // CARDS TO GENERATE RANDOM VALUES ========================
@@ -39,14 +71,16 @@ $(document).ready(function () {
         wins++;
         $("#wins").text(wins);
         $("#score").text("0");
-        $("#message").text(winMessage);
+        $("#message").text(winMessage).fadeTo(100, 1);
+        opacityToZero();
     }
 
     function lose() {
         losses++;
         $("#losses").text(losses);
         $("#score").text("0");
-        $("#message").text(loseMessage);
+        $("#message").text(loseMessage).fadeTo(100, 1);
+        opacityToZero();
     }
 
     function matchNumbers() {
@@ -59,28 +93,43 @@ $(document).ready(function () {
         }
     }
 
+    function showIntroMessage() {
+        $("#message").delay(5000).text(introMessage).fadeTo(0, 1);
+    }
+
+    function opacityToZero() {
+        setTimeout(function() {
+            $("#message").fadeTo(400, 0);
+        }, 3000);
+    }
+
     //  MAKE THE CARDS CLICKABLE AND ITS VALUES APPEAR / CONDITIONS ======================
-    $("#spades").on("click", function () {
+    $("#spades").click(function () {
         score = score + cards.spade;
         $("#score").text(score);
         matchNumbers();
     });
 
-    $("#hearts").on("click", function () {
+    $("#hearts").click(function () {
         score = score + cards.heart;
         $("#score").text(score);
         matchNumbers();
     });
-    $("#clubs").on("click", function () {
+
+    $("#clubs").click(function () {
         score = score + cards.club;
         $("#score").text(score);
         matchNumbers();
     });
 
-    $("#diamonds").on("click", function () {
+    $("#diamonds").click(function () {
         score = score + cards.diamond;
         $("#score").text(score);
         matchNumbers();
+    });
+
+    $("#game-info").click(function() {
+        $("body").css("background-color", "red");
     });
 
 });
