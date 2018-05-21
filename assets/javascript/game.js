bb59e946e2281f2edd5ff29dc1ee6345cd5d3b2a
acordafrederick <sonjaire@gmail.com>
2018-05-21 10:00:29 -0500

    added comments as a guide

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,43 +1,24 @@
 // hide the ff elements for intro
-$("#game-container, #open-info, #close-info").hide();
-$("#info-container").hide();
+$("#info-container, #game-container, #open-info, #close-info").hide();
 
 $(document).ready(function () {
 
-    $("body").addClass("logo-bg");
-    setTimeout(function() {
-        $("body").removeClass("logo-bg");
-        $("#info-container").slideToggle().show();
-    },5000);
+// GAME'S LOGIC ==========
 
-    $("#play-btn").click(function() {
-        $("#info-container").slideToggle();
-        setTimeout(function() {
-            $("#game-container, #open-info").show();
-        }, 500);
-    });
-
-    $("#open-info").click(function() {
-        $("#game-container").fadeTo(0, 0);
-        $("#play-btn, #open-info").hide();
-        $("#info-container").slideToggle().show();
-        $("#close-info").show();
-    });
-
-    $("#close-info").click(function() {
-        $("#info-container").slideToggle();
-        setTimeout(function() {
-            $("#game-container").fadeTo(0, 1);
-            $("#open-info").show();
-        }, 500);
-    })
-
-    // GENERATE RANDOM NUMBER ====================
+    // generate a random number for the player
     let number = Math.floor(Math.random() * 101 + 19);
     $("#number").text(number);
     $("#score").text("0");
 
-    // OTHER GLOBALS ======================
+    // generate random value for each card
+    let cards = {
+        "spade": Math.floor(Math.random() * 11 + 1),
+        "heart": Math.floor(Math.random() * 11 + 1),
+        "club": Math.floor(Math.random() * 11 + 1),
+        "diamond": Math.floor(Math.random() * 11 + 1)
+    }
+
+    // other global variables
     let score = 0;
     let wins = 0;
     let losses = 0;
@@ -45,15 +26,7 @@ $(document).ready(function () {
     const loseMessage = "Not a match. Try again!";
     const introMessage = "Match the Number";
 
-    // CARDS TO GENERATE RANDOM VALUES ========================
-    var cards = {
-        "spade": Math.floor(Math.random() * 11 + 1),
-        "heart": Math.floor(Math.random() * 11 + 1),
-        "club": Math.floor(Math.random() * 11 + 1),
-        "diamond": Math.floor(Math.random() * 11 + 1)
-    }
-
-    // RESET / STARTING FUNCTION
+    // function to reset the number, score, and cards' values.
     function reset() {
         number = Math.floor(Math.random() * 101 + 19);
         $("#number").text(number);
@@ -66,7 +39,7 @@ $(document).ready(function () {
         score = 0;
     }
 
-    // SET FUNCTIONS FOR WHEN THE PLAYER WINS OR LOSES
+    // function for when the player matches the number
     function winning() {
         wins++;
         $("#wins").text(wins);
@@ -75,6 +48,7 @@ $(document).ready(function () {
         opacityToZero();
     }
 
+    // function for when the player exceeds the number
     function lose() {
         losses++;
         $("#losses").text(losses);
@@ -83,6 +57,7 @@ $(document).ready(function () {
         opacityToZero();
     }
 
+    // function to flash a message accordingly if the player wins or loses a round
     function matchNumbers() {
         if (score === number) {
             winning();
@@ -93,17 +68,15 @@ $(document).ready(function () {
         }
     }
 
-    function showIntroMessage() {
-        $("#message").delay(5000).text(introMessage).fadeTo(0, 1);
-    }
-
+    // function to fade the message out to 0 opacity after 3 secs
     function opacityToZero() {
         setTimeout(function() {
             $("#message").fadeTo(400, 0);
         }, 3000);
     }
 
-    //  MAKE THE CARDS CLICKABLE AND ITS VALUES APPEAR / CONDITIONS ======================
+// USER EVENTS (GAME) ==========
+
     $("#spades").click(function () {
         score = score + cards.spade;
         $("#score").text(score);
@@ -128,8 +101,38 @@ $(document).ready(function () {
         matchNumbers();
     });
 
-    $("#game-info").click(function() {
-        $("body").css("background-color", "red");
+// USER EVENTS (INTERFACE) ==========
+
+    // intro. showcase of logo
+    $("body").addClass("logo-bg");
+    setTimeout(function() {
+        $("body").removeClass("logo-bg");
+        $("#info-container").slideToggle().show();
+    },5000);
+
+    // event to hode info and show game and info button
+    $("#play-btn").click(function() {
+        $("#info-container").slideToggle();
+        setTimeout(function() {
+            $("#game-container, #open-info").show();
+        }, 500);
+    });
+
+    // event to show info but changing the game's opacity to 0 to keep the player's stats
+    $("#open-info").click(function() {
+        $("#game-container").fadeTo(0, 0);
+        $("#play-btn, #open-info").hide();
+        $("#info-container").slideToggle().show();
+        $("#close-info").show();
+    });
+
+    // event to hide the info and changing the game's opacity back to 1
+    $("#close-info").click(function() {
+        $("#info-container").slideToggle();
+        setTimeout(function() {
+            $("#game-container").fadeTo(0, 1);
+            $("#open-info").show();
+        }, 500);
     });
 
 });
